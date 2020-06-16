4f6989d004eca03ee0b44cb07bf6ed6798f1d580
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-06-16 06:34:26 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/santosediego/workshopmongo/WorkshopmongoAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

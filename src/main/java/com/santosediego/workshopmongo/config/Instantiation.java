6e4dd24925927f386279514459c15c97c0d5d780
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-06-20 06:09:37 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/santosediego/workshopmongo/config/Instantiation.java
+++ src/main/java/com/santosediego/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.santosediego.workshopmongo.domain.Post;
 import com.santosediego.workshopmongo.domain.User;
+import com.santosediego.workshopmongo.dto.AuthorDTO;
 import com.santosediego.workshopmongo.repository.PostRepository;
 import com.santosediego.workshopmongo.repository.UserRepository;
 
@@ -30,15 +31,23 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("15/06/2020"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		postRepository.saveAll(Arrays.asList(post1, post2));
+		  User diego = new User(null, "Diego Santos", "diego@gmail.com"); User maria =
+		  new User(null, "Maria Brown", "maria@gmail.com"); User alex = new User(null,
+		  "Alex Green", "alex@gmail.com"); User bob = new User(null, "Bob Grey",
+		  "bob@gmail.com");
+		  
+		  userRepository.saveAll(Arrays.asList(diego, maria, alex, bob));
+		  
+		  Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem",
+		  "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria)); Post post2 = new
+		  Post(null, sdf.parse("15/06/2020"), "Bom dia", "Acordei feliz hoje!", new
+		  AuthorDTO(maria)); Post post3 = new Post(null, sdf.parse("20/06/2020"),
+		  "Eu sou um programador", "Tudo posso Naquele que me Fortalece!", new
+		  AuthorDTO(diego));
+		  
+		  postRepository.saveAll(Arrays.asList(post1, post2, post3));
+		 
 	}
 
 }

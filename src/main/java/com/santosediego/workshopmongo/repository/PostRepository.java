9f0e1ff772ca02caeadd85119a770453cd665fac
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2020-06-25 06:30:56 -0300

    Consulta com vários critérios

--- src/main/java/com/santosediego/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/santosediego/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.santosediego.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +16,12 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> searchTitleString(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
+/*Referências para @Query
+ * https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+ *https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+ *https://docs.mongodb.com/manual/reference/operator/query/regex/
+ **/
--- src/main/java/com/santosediego/workshopmongo/services/PostService.java
+++ src/main/java/com/santosediego/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.santosediego.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,10 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.searchTitleString(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
